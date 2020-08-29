e287574297c9f3547a8c755bc5443ac339eec916
Elliot Z. Lin <elelliot2@gmail.com>
2020-08-29 13:03:03 -0700

    Exercise 1.19 complete.

--- sicp1.scm
+++ sicp1.scm
@@ -780,7 +780,7 @@ state variables `a` and `b` in `fib-iter` process of section 1.2.2: `a <- a
 `Fib(n + 1)` and `Fib(n)`. In other words, the Fibonacci numbers are
 produced by applying `T^n`, the nth power of the transformation `T`,
 starting with the pair (1,0). Now consider `T` to be the special case of `p
-= 0` and `q = 1` in a family of transformaations `T_{pq}`, where `T_{pq}`
+= 0` and `q = 1` in a family of transformations `T_{pq}`, where `T_{pq}`
 transforms the pair `(a,b)` according to `a <- bq + aq + ap` and `b <- bp +
 aq`. Show that if we apply such a transformation `T_{pq}` twice, the effect
 is the same as using a single transformation `T_{p'q'}` of the same form,
@@ -807,4 +807,24 @@ number of steps:
 			(- count 1)))))
 |#
 
-ANSWER
+;; Compute p'
+(+ (square p) (square q))
+
+;; Compute q'
+(+ (* 2 p q) (square q))
+
+;; Explanation: Use the transformation for b to compute p' and q'.
+;; Given:
+;;   a' = bq + aq + ap
+;;   b' = bp + aq
+;;
+;; b'' = b'p + a'q
+;;     = (bp + aq)p + (bq + aq + ap)q
+;;     = bp^2 + apq + bq^2 + aq^2 + apq
+;;     = bp^2 + bq^2 + 2apq + aq^2          ; Group terms
+;;     = b(p^2 + q^2) + a(2pq + q^2)
+;;     = bp' + aq'
+;;
+;; Therefore:
+;;   p' = p^2 + q^2
+;;   q' = 2pq + q^2
