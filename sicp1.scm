5ee23ec1fbf21bd54d55c05b2284361a6fa52a6f
Elliot Z. Lin <elelliot2@gmail.com>
2020-09-16 20:49:19 -0700

    Add questions exercise 1.29-1.33

--- sicp1.scm
+++ sicp1.scm
@@ -1271,3 +1271,102 @@ return 0.
 (fast-prime? 1729 100) ; #f
 (fast-prime? 2465 100) ; #f
 (fast-prime? 6601 100) ; #f
+
+
+#| Exercise 1.29 Simpson's Rule is a more accurate method of numerical
+integration than the method illustrated above. Using Simpson's Rule, the
+integral of a function f between a and b is approximated as:
+
+h / 3 [y_0 + 4y_1 + 2y_2 + y_3 + 2y_4 + ... + 2y_{n-2} + 4y_{n-1} + y_n]
+
+where `h = (b - a)/n`, for some even integer `n`, and `y_k = f(a +
+kh)`. (Increasing `n` increases the accuracy of the approximation.) Define
+a procedure that takes as arguments `f`, `a`, `b`, and `n` and returns the
+value of the integral, computed using Simpson's Rule. Use your procedure to
+integrate `cube` between 0 and 1 (with n = 100 and n = 1000), and compare
+the results to those of the `integral` procedure shown above.
+|#
+
+ANSWER
+
+
+#| Exercise 1.30 The sum procedure above generates a linear recursion. The
+procedure can be rewritten so that the sum is performed iteratively. Show
+how to do this by filling in the missing expressions in the following
+definition:
+
+```
+(define (sum term a next b)
+  (define (iter a result)
+    (if <??>
+        <??>
+        (iter <??> <??>)))
+  (iter <??> <??>))
+```
+|#
+
+ANSWER
+
+
+#| Exercise 1.31
+a. The sum procedure is only the simplest of a vast number of similar
+abstractions that can be captured as higher-order procedures. Write an
+analogous procedure called `product` that returns the product of the values
+of a function at points over a given range. Show how to define `factorial`
+in terms of `product`. Also use product to compute approximations to `pi`
+using the formula:
+
+pi = 2 * 4 * 4 * 6 * 6 * 8 ...
+--   -------------------------
+4    3 * 3 * 5 * 5 * 7 * 7 ...
+
+b. If your `product` procedure generates a recursive process, write one
+that generates an iterative process. If it generates an iterative process,
+write one that generates a recursive process.
+|#
+
+ANSWER
+
+
+#| Exercise 1.32
+a. Show that `sum` and `product` (exercise 1.31) are both special cases of
+a still more general notion called `accumulate` that combins a collection
+of terms, using some general accumulation function:
+
+```
+(accumulate combiner null-value term a next b)
+```
+
+`accumulate` takes as arguments the same term and range specifications as
+`sum` and `product`, together with a `combiner` procedure (of two
+arguments) that specifies how the current term is to be combined with the
+accumulation of the preceding terms and a `null-value` that specifies what
+base value to use when the terms run out. Write `accumulate` and show how
+`sum` and `product` can both be defined as simple calls to `accumulate`.
+
+b. If your `accumulate` procedure generates a recursive process, write one
+that generates an iterative process. If it generates an iterative process,
+write one that generates a recursive process.
+|#
+
+ANSWER
+
+
+#| Exercise 1.33 You can obtain an even more general version of
+`accumulate` (exercise 1.32) by introducing the notion of a filter on the
+terms to be combined. That is, combine only those terms derived from values
+in the range that satisfy a specified condition. The resulting
+`filtered-accumulate` abstraction takes the same arguments as accumulate,
+together with an additional predicate of one argument that specifies the
+filter. Write `filtered-accumulate` as a procedure. Show how to express the
+following using `filtered-accumulate1`:
+
+a. the sum of the squares of the prime numbers in the interval `a` to
+`b` (assuming that you have a `prime?` predicate already written)
+
+b. the product of all the positive integers less than `n` that are
+relatively prime to `n` (i.e., all positive integers `i < n` such that
+`GCD(i,n) = 1`).
+|#
+
+ANSWER
