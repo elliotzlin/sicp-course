189326825af2c3cb1f188e55be226691fc661d61
Elliot Z. Lin <elelliot2@gmail.com>
2020-09-20 21:15:28 -0700

    Complete exercise 1.29

--- sicp1.scm
+++ sicp1.scm
@@ -1277,7 +1277,7 @@ return 0.
 integration than the method illustrated above. Using Simpson's Rule, the
 integral of a function f between a and b is approximated as:
 
-h / 3 [y_0 + 4y_1 + 2y_2 + y_3 + 2y_4 + ... + 2y_{n-2} + 4y_{n-1} + y_n]
+h / 3 [y_0 + 4y_1 + 2y_2 + 4y_3 + 2y_4 + ... + 2y_{n-2} + 4y_{n-1} + y_n]
 
 where `h = (b - a)/n`, for some even integer `n`, and `y_k = f(a +
 kh)`. (Increasing `n` increases the accuracy of the approximation.) Define
@@ -1287,7 +1287,25 @@ integrate `cube` between 0 and 1 (with n = 100 and n = 1000), and compare
 the results to those of the `integral` procedure shown above.
 |#
 
-ANSWER
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (inc n) (+ n 1))
+
+(define (simpsons-integral f a b n)
+  (define h (/ (- b a) n))
+  (define (simpsons-term k)
+    (define y (f (+ a (* k h))))
+    (cond ((or (= k 0) (= k n)) y)
+	  ((even? k) (* 2 y))
+	  (else (* 4 y))))
+  (* (/ h 3) (sum simpsons-term 0 inc n)))
+
+(simpsons-integral cube 0 1 100)  ; 1/4
+(simpsons-integral cube 0 1 1000) ; 1/4
 
 
 #| Exercise 1.30 The sum procedure above generates a linear recursion. The
