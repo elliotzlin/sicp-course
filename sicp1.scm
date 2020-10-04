4201c99a5bc5766841b37507a2a52b24401fd25d
Elliot Z. Lin <elelliot2@gmail.com>
2020-10-04 15:20:35 -0700

    Add questions for exercises 1.35 to 1.39

--- sicp1.scm
+++ sicp1.scm
@@ -1525,3 +1525,103 @@ If we call `(f f)`, it will call `(f 2)`, which calls `(2 2)`, which will
 error because `2` is not a callable function.
 
 Exact error: `The object 2 is not applicable.`
+
+
+#| Exercise 1.35 Show that the golden ratio `phi` (section 1.2.2) is a
+fixed point of the transformation `x |-> 1 + 1/x`, and use this fact to
+compute by means of the `fixed-point` procedure.
+|#
+
+ANSWER
+
+
+#| Exercise 1.36 Modify `fixed-point` so that it prints the sequence of
+approximations it generates, using the newline and display primitives shown
+in exercise 1.22. Then find a solution to `x^x = 1000` by finding a fixed
+point of `x |-> log(1000)/log(x)`. (Use Scheme's primitive `log` procedure,
+which computes natural logarithms.) Compare the number of steps this takes
+with and without average damping. (Note that you cannot start `fixed-point`
+with a guess of 1, as this would cause division by `log(1) = 0`.)\
+|#
+
+ANSWER
+
+
+#| Exercise 1.37
+a. An infinite `continued fraction` is an expression of the form
+```
+f =         N_1
+    _____________________
+    D_1 +       N_2
+          _______________
+          D_2 +    N_3
+                _________
+                D_3 + ...
+```
+As an example, one can show that the infinite continued fraction expansion
+with the `N_i` and the `D_i` all equal to 1 produces `1/phi`, where `phi`
+is the golden ratio (described in section 1.2.2). One way to approximate an
+infinite continued fraction is to truncate the expansion after a given
+number of terms. Such a truncation -- a so-called `k-term finite continued
+fraction` -- has the form
+```
+        N_1
+__________________
+D_1 +     N_2
+     _____________
+     .
+      .
+       . + N_K
+           ___
+           D_K
+```
+Suppose that `n` and `d` are procedures of one argument (the term index
+`i`) that return the `N_i` and `D_i` of the terms of the continued
+fraction. Define a procedure `cont-frac` such that evaluating `(cont-frac n
+d k)` computes the value of the `k`-term finite continued fraction. Check
+your procedure by approximating `1/phi` using
+```
+(cont-frac (lambda (i) 1.0)
+           (lambda (i) 1.0)
+           k)
+```
+for successive values of `k`. How large must you make `k` in order to get
+an approximation that is accurate to 4 decimal places?
+
+b. If your `cont-frac` procedure generates a recursive process, write one
+that generates an iterative process. If it generates an iterative process,
+write one that generates a recursive process.
+|#
+
+ANSWER
+
+
+#| Exercise 1.38 In 1737, the Swiss mathematician Leonhard Euler published
+a memoir `De Fractionibus Continuis`, which included a continued fraction
+expansion for `e - 2`, where `e` is the base of the natural logarithms. In
+this fraction, the `N_i` are all 1, and the `D_i` are successively 1, 2, 1,
+1, 4, 1, 1, 6, 1, 1, 8, .... Write a program that uses your `cont-frac`
+procedure from exercise 1.37 to approximate `e`, based on Euler's
+expansion.
+|#
+
+ANSWER
+
+
+#| Exercise 1.39 A continued fraction representation of the tangent
+function was published in 1770 by the German mathematician J.H. Lambert:
+```
+tan(r) =        r
+         _______________
+         1 -     r^2
+             ___________
+             3 -   r^2
+                 _______
+                 5 - ...
+```
+where `x` is in radians. Define a procedure `(tan-cf x k)` that computes an
+approximation to the tangent function based on Lambert's formula. `K`
+specifies the number of terms to compute, as in exercise 1.37.
+|#
+
+ANSWER
